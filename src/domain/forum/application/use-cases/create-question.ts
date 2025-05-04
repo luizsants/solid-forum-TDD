73255c8cdd09af615f1f7e1d2f9bdde86c64cdef
LuizSants <luizfernando.santos900@gmail.com>
2025-05-04 15:01:54 -0300

    added watched list 1.0

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,13 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { Either, right } from '@/core/either'
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 interface CreateQuestionUseCaseRequest {
   authorId: string
   title: string
   content: string
+  attachmentsIds: string[]
 }
 type CreateQuestionUseCaseResponse = Either<
   null,
@@ -22,6 +24,7 @@ export class CreateQuestionUseCase {
     authorId,
     title,
     content,
+    attachmentsIds,
   }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
     const question = Question.create({
       authorId: new UniqueEntityID(authorId),
@@ -29,7 +32,17 @@ export class CreateQuestionUseCase {
       content,
     })
 
+    const questionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      })
+    })
+
+    question.attachments = questionAttachments
+
     await this.questionsRepository.create(question)
+
     return right({
       question,
     })
